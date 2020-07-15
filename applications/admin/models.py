6f0ca5cb818de51431a114812d2fa5450485a981
catVSdog <windorbird@gmail.com>
2020-07-15 23:53:42 +0800

    优化model

--- applications/admin/models.py
+++ applications/admin/models.py
@@ -1,17 +1,21 @@
+from flask_login import UserMixin
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship
+
 from applications.admin.password_handler import Argon2Handler
 from database.db import BaseModel
-from flask_login import UserMixin
-from sqlalchemy import Column, DateTime, String
 
 
 class User(UserMixin, BaseModel):
     username = Column(String(255), unique=True, nullable=False)
+    profile_picture = Column(Text)
     password = Column(String(255), nullable=False)
     email = Column(String(255), unique=True, nullable=False)
     last_login = Column(DateTime)
+    role_id = Column(Integer, ForeignKey('Role.id'))
 
-    def __repr__(self):
-        return f'<User {self.username}>'
+    def __str__(self):
+        return f'<User: {self.username}>'
 
     # store raw password
     _password = None
@@ -20,3 +24,15 @@ class User(UserMixin, BaseModel):
         password_handler = Argon2Handler()
         self.password = password_handler.encode(row_password)
         self._password = row_password
+
+    def check_password(self, row_password):
+        pass
+
+
+class Role(BaseModel):
+    name = Column(String(50), unique=True, nullable=True)
+    color = Column(String(20), unique=True, nullable=True)
+    user = relationship('User', backref='role', lazy=True)
+
+    def __str__(self):
+        return f'<Role: {self.name}>'
