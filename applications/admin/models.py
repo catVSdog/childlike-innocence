b101b6d38e7dc86f12431e393a616a150452ad67
catVSdog <windorbird@gmail.com>
2020-07-20 23:05:33 +0800

    重新设计用户，权限表，支持多种登陆方式

--- applications/admin/models.py
+++ applications/admin/models.py
@@ -1,23 +1,31 @@
 from flask_login import UserMixin
 from sqlalchemy import (Boolean, Column, DateTime, ForeignKey, Integer, String,
                         Text)
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import backref, relationship
 
 from applications.admin.password_handler import Argon2Handler
 from database.db import BaseModel
 
 
 class User(UserMixin, BaseModel):
-    username = Column(String(255), unique=True, nullable=False)
+    nicknanme = Column(String(30), nullable=False, default='匿名用户')
     profile_picture = Column(Text)
-    password = Column(String(255), nullable=False)
-    email = Column(String(255), unique=True, nullable=False)
     last_login = Column(DateTime)
     role_id = Column(Integer, ForeignKey('role.id'))
-    validated = Column(Boolean, default=False)
 
     def __str__(self):
-        return f'<User: {self.username}>'
+        return f'<User: {self.nicknanme}>'
+
+
+class BasicAuth(BaseModel):
+    username = Column(String(255), unique=True)
+    mobile = Column(String(11), unique=True)
+    email = Column(String(255), unique=True)
+    password = Column(String(255), nullable=False)
+    email_is_validated = Column(Boolean, default=False)
+    mobile_is_validated = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', backref=backref('basic', uselist=False), lazy=True)
 
     password_handler = Argon2Handler()
 
@@ -28,9 +36,20 @@ class User(UserMixin, BaseModel):
         return self.password_handler.verify(row_password, self.password)
 
 
+class OauthAuth(BaseModel):
+    third_type = Column(Integer)
+    auth_name = Column(String(255), unique=True)
+    auth_token = Column(String(20), unique=True)
+    is_validated = Column(Boolean, default=False)
+    expires = Column(Integer)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', backref=backref('oauth', uselist=False), lazy=True)
+
+
 class Role(BaseModel):
     name = Column(String(50), unique=True, nullable=True)
     color = Column(String(20), unique=True, nullable=True)
+    description = Column(String(255))  # 描述角色权限
     user = relationship('User', backref='role', lazy=True)
 
     def __str__(self):
